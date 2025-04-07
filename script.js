4f091895fb9eba6bd5e95712213aed6fc0a0804f
Jason Brown <jason.s.brown3141@gmail.com>
2025-04-07 09:23:30 -0400

    filter whitespace from upload

--- script.js
+++ script.js
@@ -70,7 +70,10 @@ fileInput.addEventListener("change", (event) => {
 function handleCSV(file) {
     Papa.parse(file, {
         complete: function (results) {
-            const zipCodes = results.data[0];  // Assuming zip codes are in the first row
+            const zipCodes = results.data[0]
+                .map(zip => zip.trim())
+                .filter(zip => zip.length > 0);  // Assuming zip codes are in the first row
+
             fetchPolygonsForZipCodes(zipCodes);  // Fetch and plot polygons for all zip codes
         },
         header: false
